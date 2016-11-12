dc4d3c0d5b1061cb54cccd28a2385faf16db2302
Joyce Au <jawt94@gmail.com>
2016-11-12 17:08:34 -0500

    updated todo_list files

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,23 @@
+class TodoList
+
+	def initialize(array)
+		@items = array 
+	end 
+
+	def get_items
+		@items 
+	end 
+
+	def add_item(item)
+		@items << item 
+	end 
+
+	def delete_item(item)
+		@items.delete(item)
+	end 
+
+	def get_item(index)
+		@items[index]
+	end 
+end 
+
